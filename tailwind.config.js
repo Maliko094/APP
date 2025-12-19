71644d9916fc15342ec5e1cbad1cc9aa93540e61
Maliko094 <oliver_demorais@hotmail.dk>
2025-12-19 14:42:03 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f172a',
+        accent: '#2563eb',
+        success: '#16a34a',
+        danger: '#dc2626'
+      }
+    }
+  },
+  plugins: [],
+}
